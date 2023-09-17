61c2a32c977ee9fff3e8e42a1ff9e6cc36e82595
ROHAN CHAKRABORTY <140347281+RohanRival@users.noreply.github.com>
2023-09-18 01:49:43 +0530

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,9 +6,7 @@ const Navbar = () => {
   const navigate = useNavigate();
 
   const handleLogout = () => {
-    // Implement your logout logic here
-    // For example, clear user session, remove tokens, etc.
-    // After logout, navigate to the login page
+    
     navigate('/');
   };
 
